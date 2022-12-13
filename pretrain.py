ca0fa2ab9521fc59eedd29b2e8b2c5f3e4443d42
Benjamin <benjamin.barda@hcl.com>
2022-12-13 13:41:08 +0100

    low acc

--- pretrain.py
+++ pretrain.py
@@ -6,6 +6,7 @@ import torchvision.datasets as datasets
 import torchvision.transforms as transforms
 
 import json
+import argparse
 
 from torch.utils.data import DataLoader, Subset
 
@@ -13,110 +14,144 @@ from models.backbone import BackBoneEncoder
 from utils.schedulers import cosine_decay_scheduler
 from utils.meters import AverageMeter
 
+parser = argparse.ArgumentParser(description="Pretrain the backbone")
 
-device = "cuda" if torch.cuda.is_available() else "cpu"
+parser.add_argument(
+    "--config",
+    help="path to the config file, any command line argument will override the config file",
+)
+parser.add_argument(
+    "--device",  help="Whant device to use", choices=["cpu", "gpu"]
+)
+parser.add_argument(
+    "--model",
+    help="Choose the backbone for the simsiam network",
+    choices=["resnet18", "resnet50"],
+)
+parser.add_argument("--encoder_dim", help="Output dimension for the encoder")
+parser.add_argument("--pred_dim", help="Output dimension for the predictor")
+
+args = parser.parse_args()
+
+device = "cuda" if torch.cuda.is_available() and args.device == "gpu" else "cpu"
 print(f"[ + ] Device set to: {device}")
 
 
-class TwoCropTransform : 
+class TwoCropTransform:
+    def __init__(self, base_transform):
+        self.base_transform = base_transform
 
-  def __init__(self, base_transform) : 
-    self.base_transform = base_transform
+    def __call__(self, image):
+        q = self.base_transform(image)
+        k = self.base_transform(image)
 
-  def __call__(self, image) : 
-    q = self.base_transform(image)
-    k = self.base_transform(image)
-
-    return [q, k]
+        return [q, k]
 
 
 def train(model, criterion, optimizer, loader, epoch):
-  losses = AverageMeter("Loss",  ":.4f") 
+    losses = AverageMeter("Loss", ":.4f")
 
-  for _, (images, _) in enumerate(loader):
+    for _, (images, _) in enumerate(loader):
 
-    images[0] = images[0].to(device, non_blocking = True)
-    images[1] = images[1].to(device, non_blocking = True)
-    
-    p1, p2, z1, z2 = model(images[0], images[1])
+        images[0] = images[0].to(device, non_blocking=True)
+        images[1] = images[1].to(device, non_blocking=True)
 
-    loss = -(criterion(p1, z2).mean() + criterion(p2, z1).mean()) * 0.5
+        p1, p2, z1, z2 = model(images[0], images[1])
 
-    losses.update(loss.item(), images[0].shape[0])
+        loss = -(criterion(p1, z2).mean() + criterion(p2, z1).mean()) * 0.5
 
-    optimizer.zero_grad()
-    loss.backward()
-    optimizer.step()
+        losses.update(loss.item(), images[0].shape[0])
 
-  print(f"Epoch : {epoch} , {losses}")
-  return losses.avg
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
 
-
-def main(): 
-
-  with open("pretrain.config.json", "r") as f :
-    config = json.load(f)
-
-  model_config = config["model"]
-  model = BackBoneEncoder(models.__dict__[model_config["architecture"]], model_config["encoder_dim"], model_config["pred_dim"], in_pretrain=True).to(device)
-
-  augmentation = [
-      transforms.RandomResizedCrop(28, scale=(.2, 1)),
-      transforms.RandomApply([transforms.ColorJitter(.4, .4, .4, .1)], p = .6),
-      transforms.RandomHorizontalFlip(),
-      transforms.RandomGrayscale(p = 2),
-      transforms.ToTensor()
-  ]
-
-  batch_size = config["trainer"]["batch_size"]
-  current_epoch = 0
-  num_epochs = config["trainer"]["max_epochs"]
-
-  save_step = config["trainer"]["save_step"]
-  
-  train_data = datasets.CIFAR10(root = "data", train = True, download = True, transform = TwoCropTransform(transforms.Compose(augmentation)))
-  idx = [x for x in range(batch_size)]
-  train_data = Subset(train_data, idx)
-  loader = DataLoader(train_data, batch_size = batch_size, shuffle=True)
-
-  criterion = nn.CosineSimilarity(dim = 1).to("cuda")
-
-  optim_config = config["optimizer"]
-  optimizer = optim.SGD(model.parameters(), lr=optim_config["lr"], momentum = optim_config["momentum"], weight_decay=optim_config["weight_decay"])
+    print(f"Epoch : {epoch} , {losses}")
+    return losses.avg
 
 
-  if config["checkpoint"] : 
+def main():
 
-    checkpoint = torch.load(config["path_to_checkpoint"])
-    model.load_state_dict(checkpoint["model"])
-    optimizer.load_state_dict(checkpoint["optimizer"])
-    current_epoch = checkpoint["epoch"]
-    loss = checkpoint["loss"]
+    with open(args.config, "r") as f:
+        config = json.load(f)
 
-    print(f"Resuming Training from Epoch {current_epoch}, Last Loss {loss}")
-  
-  model.train()
+    model_config = config["model"]
 
-  for epoch in range(current_epoch, num_epochs):
-    print(f"Epoch {epoch}")
-    avg_epoch_loss = train(model, criterion, optimizer, loader, epoch)
-    cosine_decay_scheduler(optimizer, .05, epoch, num_epochs )
-    
-    
-    torch.save({
-      "epoch" : epoch, 
-      "model" : model.state_dict(), 
-      "optimizer": optimizer.state_dict(),
-      "loss" : avg_epoch_loss
-      }, config["path_to_checkpoint"])
+    model = BackBoneEncoder(
+        models.__dict__[model_config["architecture"]]
+        if args.model is None
+        else args.model,
+        model_config["encoder_dim"] if args.encoder_dim is None else args.encoder_dim,
+        model_config["pred_dim"] if args.pred_dim is None else args.pred_dim,
+        in_pretrain=True,
+    ).to(device)
 
-    config["trainer"]["current_epoch"] = epoch + 1 
-    config["checkpoint"] = True
+    augmentation = [
+        transforms.RandomResizedCrop(28, scale=(0.2, 1)),
+        transforms.RandomApply([transforms.ColorJitter(0.4, 0.4, 0.4, 0.1)], p=0.6),
+        transforms.RandomHorizontalFlip(),
+        transforms.RandomGrayscale(p=2),
+        transforms.ToTensor(),
+    ]
 
-    with open("pretrain.config.json", "w") as out : 
-      json.dump(config, out, indent=4)
-    print("Checkpoint Saved")
-    
+    batch_size = config["trainer"]["batch_size"]
+    current_epoch = 0
+    num_epochs = config["trainer"]["max_epochs"]
 
-if __name__ == "__main__" :
-  main()  
+    train_data = datasets.CIFAR10(
+        root="data",
+        train=True,
+        download=True,
+        transform=TwoCropTransform(transforms.Compose(augmentation)),
+    )
+    idx = [x for x in range(batch_size)]
+    train_data = Subset(train_data, idx)
+    loader = DataLoader(train_data, batch_size=batch_size, shuffle=True)
+
+    criterion = nn.CosineSimilarity(dim=1).to("cuda")
+
+    optim_config = config["optimizer"]
+    optimizer = optim.SGD(
+        model.parameters(),
+        lr=optim_config["lr"],
+        momentum=optim_config["momentum"],
+        weight_decay=optim_config["weight_decay"],
+    )
+
+    if config["checkpoint"]:
+
+        checkpoint = torch.load(config["path_to_checkpoint"])
+        model.load_state_dict(checkpoint["model"])
+        optimizer.load_state_dict(checkpoint["optimizer"])
+        current_epoch = checkpoint["epoch"]
+        loss = checkpoint["loss"]
+
+        print(f"Resuming Training from Epoch {current_epoch}, Last Loss {loss}")
+
+    model.train()
+
+    for epoch in range(current_epoch, num_epochs):
+        print(f"Epoch {epoch}")
+        avg_epoch_loss = train(model, criterion, optimizer, loader, epoch)
+        cosine_decay_scheduler(optimizer, 0.05, epoch, num_epochs)
+
+        torch.save(
+            {
+                "epoch": epoch,
+                "model": model.state_dict(),
+                "optimizer": optimizer.state_dict(),
+                "loss": avg_epoch_loss,
+            },
+            config["path_to_checkpoint"],
+        )
+
+        config["trainer"]["current_epoch"] = epoch + 1
+        config["checkpoint"] = True
+
+        with open("pretrain.config.json", "w") as out:
+            json.dump(config, out, indent=4)
+        print("Checkpoint Saved")
+
+
+if __name__ == "__main__":
+    main()
--- utils/lcls_cfg.py
+++ utils/lcls_cfg.py
@@ -7,14 +7,14 @@ cfg = __C
 __C.initial_budget = .1
 
 # Percentage of whole dataset to label before stop labeling
-__C.final_budget = 0.5
+__C.final_budget = 0.8
 
 # How many sample to add evry cycle
-__C.b = .004
+__C.b = .005
 
 # Stage2 training parameters
-__C.stage2_bs = 100
-__C.stage2_lr = 30
+__C.stage2_bs = 32
+__C.stage2_lr = 2
 __C.stage2_num_workers = 1
 __C.stage2_momentum = .9
 __C.stage2_weigth_decay = 0
